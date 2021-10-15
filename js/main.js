c33c5d64d6af7be02c109e676256c77a46dbdaa8
achemakin <kotor79@gmail.com>
2021-10-16 00:57:33 +0300

    add sections Catalog and Events

--- js/main.js
+++ js/main.js
@@ -12,6 +12,26 @@ const modalsEl = document.querySelector('.js-modals');
 const modals = document.querySelectorAll('.js-modal');
 const widthScroll = window.innerWidth - document.body.offsetWidth + 'px';
 
+const accordionsEl = document.querySelectorAll('.js-accordion');
+// Для открытие секции аккордиона при загрузке сайта
+if (accordionsEl) {
+  accordionsEl.forEach(el => {
+    if (el.classList.contains('accordion--active')) {
+      el.lastElementChild.style.maxHeight = el.lastElementChild.scrollHeight + 'px';
+    }
+  });
+}
+
+// Элементы для работы табов в каталоге
+const accContentBtnEl = document.querySelectorAll('.js-acc-content-btn');
+const tabArticleEl = document.querySelectorAll('.js-tab-article');
+const tabsBtnEl = document.querySelectorAll('.js-tabs-btn');
+const catalogTabEl = document.querySelectorAll('.js-catalog-tab');
+
+// Для открытие всех карточек при нажатии на кнопку все события
+const eventsSlideEl = document.querySelectorAll('.js-events-slide');
+
+// Открытие модального окна
 function openModals(modalId) {
   const pagePosition = window.scrollY;
   bodyEl.dataset.position = pagePosition;
@@ -28,6 +48,7 @@ function openModals(modalId) {
   })
 }
 
+// Закрытие модального окна
 function closeModals() {
   const pagePosition = parseInt(bodyEl.dataset.position, 10);
   const activeModalEl = modalsEl.querySelector('.active');
@@ -50,6 +71,7 @@ function closeBurger() {
 
 document.addEventListener('click', event => {
     const target = event.target;
+    console.log(target);
 
     // Открытие/закрытие меню при нажатии кнопки меню или за границами окна меню
     const submenuOpen = headerMenuEl.querySelector('.submenu-open');
@@ -106,6 +128,85 @@ document.addEventListener('click', event => {
     if (target === searchCloseEl) {
       headerBottomEl.classList.remove('search--open');
     }
+
+    // открытие/закрытие accordion
+    const isAccordion = target.classList.contains('js-accordion-btn');
+    if (isAccordion) {      
+      const isActiveAccordion = target.parentElement.classList.contains('accordion--active'); 
+     
+      accordionsEl.forEach(el => {
+        if (el.classList.contains('accordion--active')) {
+          el.classList.remove('accordion--active');
+          el.lastElementChild.style.maxHeight = null;
+          el.firstElementChild.setAttribute('aria-expanded', 'false');
+          el.lastElementChild.setAttribute('aria-hidden', 'true');
+        }
+      })
+
+      if (!isActiveAccordion) {
+        target.parentElement.classList.add('accordion--active');
+        target.nextElementSibling.style.maxHeight = target.nextElementSibling.scrollHeight + 'px';
+        target.setAttribute('aria-expanded', 'true');
+        target.nextElementSibling.setAttribute('aria-hidden', 'false');
+      }
+    }
+
+    //выбор автора в accordion__item
+    const isAccContentBtn = target.classList.contains('js-acc-content-btn');
+    if (isAccContentBtn) {
+      accContentBtnEl.forEach(el => {
+        if (el.classList.contains('accordion__content-btn--active')) {          
+          el.classList.remove('accordion__content-btn--active');
+        }
+      })
+
+      tabArticleEl.forEach(el => {
+        el.classList.remove('tab__article--active');
+
+        if (el.getAttribute('data-target') === target.getAttribute('data-path')) {
+          el.classList.add('tab__article--active');
+          if (el.querySelector('.js-img-name') != null) el.querySelector('.js-img-name').innerHTML = target.innerHTML;
+        }   
+      })
+
+      target.classList.add('accordion__content-btn--active');
+
+      if (window.matchMedia("(max-width: 991px)").matches) {
+        window.scrollTo({
+            top: document.getElementById('tab-left').getBoundingClientRect().top + window.scrollY - 50,
+            behavior: "smooth"
+        });
+      }
+    }
+
+    // Переключение табов
+    const isTabsBtn = target.classList.contains('js-tabs-btn');
+    if (isTabsBtn) {
+      tabsBtnEl.forEach(el => {
+        el.classList.remove('tabs__btn--active');
+      })
+
+      target.classList.add('tabs__btn--active');
+
+      catalogTabEl.forEach(el => {
+        el.classList.remove('tab--active');
+        if (el.getAttribute('data-tab-target') === target.getAttribute('data-tab-path')) {
+          el.classList.add('tab--active');
+        }
+      })
+    }
+
+    // Нажатие кнопки Все события
+    const isBtnAll = target.classList.contains('js-events-all');
+    if (isBtnAll) {
+      eventsSlideEl.forEach(el => {
+        if (getComputedStyle(el).display === 'none') {
+          el.style.display = 'flex';
+        }
+      })
+
+      target.style.display = 'none';
+    }
 });
 
 // Закрытие меню при скролле
@@ -136,8 +237,8 @@ document.body.addEventListener('keyup', event => {
   }
 });
 
-// Swiper
-const gallrySwiper = new Swiper('.js-gallery-swiper', {
+// Gallery-swiper
+const gallerySwiper = new Swiper('.js-gallery-swiper', {
   speed: 500,
   spaceBetween: 34,
   slidesPerView: 1,
@@ -148,13 +249,13 @@ const gallrySwiper = new Swiper('.js-gallery-swiper', {
   },
   
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination--gallery',
     type: 'fraction',
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next--gallery',
+    prevEl: '.swiper-button-prev--gallery',
   },
 
   breakpoints: {
@@ -177,6 +278,35 @@ const gallrySwiper = new Swiper('.js-gallery-swiper', {
   }
 });
 
+// Events-swiper
+let eventsSwiper = new Swiper('.js-events-swiper', {
+  speed: 500,
+  spaceBetween: 27,
+  pagination: {
+    el: '.events__swiper-pagination',
+  },
+});
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  eventsSwiper.destroy();
+}
+
+window.addEventListener('resize', () => {
+  if (window.matchMedia("(min-width: 768px)").matches && !eventsSwiper.destroyed) {
+      eventsSwiper.destroy();
+    }
+
+  if (window.matchMedia("(max-width: 767px)").matches && eventsSwiper.destroyed) {
+    eventsSwiper = new Swiper('.js-events-swiper', {
+      speed: 500,
+      spaceBetween: 27,
+      pagination: {
+        el: '.events__swiper-pagination',
+      },
+    });
+  }  
+});
+
 //Choices
 const element = document.querySelector('.js-gallery-choice');
 const choices = new Choices(element, {
@@ -196,7 +326,7 @@ scrollEl.forEach(el => new SimpleBar(el,
 ));
 
 
-// Media query
+// Search Button
 if (window.matchMedia("(max-width: 1399px)").matches) {  
   searchBtnEl.removeAttribute('disabled');
   searchBtnEl.nextElementSibling.removeAttribute('placeholder');
